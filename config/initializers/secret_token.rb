06f0e2f357bab1722cd705d0e7cce869d5fad82b
Vincent Leung <vincentleung1991@gmail.com>
2012-09-21 19:10:56 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sublease::Application.config.secret_token = '76bf63c1e62971e89ca5041419266724a9a76d7205c2b929368c40f048058d1529fca0cce31c202162329776f516d2eb8430cdae8e9f7703a6a8adebcf63c65d'
