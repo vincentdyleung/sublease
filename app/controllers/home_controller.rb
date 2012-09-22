0538329e3d4734d44d1091281db8436b5d37e301
Andrew <andycho7@gmail.com>
2012-09-22 00:53:12 -0400

    Home page update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  layout :index
   def index
+    
   end
 end
---
+++ db/migrate/20120922022231_add_fields_to_residence.rb
@@ -0,0 +1,4 @@
+class AddFieldsToResidence < ActiveRecord::Migration
+  def change
+  end
+end
